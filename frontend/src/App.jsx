6e279c7bcfbcfb96ef83f1f2352346e6eb5e51d6
RahulSidar08 <rahulsidar2056@gmail.com>
2025-03-29 22:03:40 +0530

    routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,51 +1,25 @@
 import { useState } from 'react'
 import './App.css'
 import { Button, TextField, Card, CardContent } from "@mui/material";
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { Login } from './authentication/Login';
 import { Signup } from './authentication/Signup';
 import { ToastContainer } from "react-toastify";
 import UsersList from './components/FetchUsers';
 import EditUser from './components/EditUser';
 import ProtectedRoute from './components/ProtectedRoute';
-const appRouter = createBrowserRouter([
-  {
-    path : "/",
-    element : <div>
-      <Login/>
-    </div>
-  },
-  {
-    path : "/signup",
-    element : <div>
-      <Signup/>
-    </div>
-  },
-  {
-    path : "/users",
-    element : <div>
-      <ProtectedRoute>
-      <UsersList/>
-      </ProtectedRoute>
-    </div>
-  },
-  {
-    path : "/edit/:id",
-    element : <div>
-      <ProtectedRoute>
-      <EditUser/>
-      </ProtectedRoute>
-    </div>
-  },
-
-])
 
 function App() {
   return (
     <>
-    <div>
-      <RouterProvider router={appRouter}/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/users" element={<ProtectedRoute><UsersList /></ProtectedRoute>} />
+        <Route path="/edit/:id" element={<ProtectedRoute><EditUser /></ProtectedRoute>} />
+      </Routes>
+    </Router>
     <ToastContainer/>
     </>
   )
